142e8cb2ea50b1ebe069ce8276e65bca3905aa14
suragch <studymongolian@gmail.com>
2021-04-12 17:01:31 +0800

    light default

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 var themeNotifier = ValueNotifier<ThemeVariation>(
-  ThemeVariation(Colors.blue, Brightness.dark),
+  ThemeVariation(Colors.blue, Brightness.light),
 );
 
 class ThemeVariation {
